942bb426bd4cf451b2b92d56b8e4570f60aa1a81
Yotta <seearomi@outlook.com>
2021-03-08 19:58:11 +0900

    v1

---
+++ WikiBulkInsert/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace WikiBulkInsert
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPw = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDb = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnTxtFileBrowse = new System.Windows.Forms.Button();
+            this.btnFileCheck = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTableNameBase = new System.Windows.Forms.TextBox();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Postgresql Server";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(127, 18);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(138, 23);
+            this.txtHost.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(315, 19);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(63, 23);
+            this.txtPort.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(398, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(426, 19);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 23);
+            this.txtId.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(539, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password";
+            // 
+            // txtPw
+            // 
+            this.txtPw.Location = new System.Drawing.Point(600, 19);
+            this.txtPw.Name = "txtPw";
+            this.txtPw.Size = new System.Drawing.Size(100, 23);
+            this.txtPw.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(715, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Database";
+            // 
+            // txtDb
+            // 
+            this.txtDb.Location = new System.Drawing.Point(777, 19);
+            this.txtDb.Name = "txtDb";
+            this.txtDb.Size = new System.Drawing.Size(100, 23);
+            this.txtDb.TabIndex = 9;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(893, 19);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(81, 23);
+            this.btnConnect.TabIndex = 10;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(980, 19);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 11;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Export file";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(90, 62);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(610, 23);
+            this.txtFilePath.TabIndex = 13;
+            // 
+            // btnTxtFileBrowse
+            // 
+            this.btnTxtFileBrowse.Location = new System.Drawing.Point(706, 61);
+            this.btnTxtFileBrowse.Name = "btnTxtFileBrowse";
+            this.btnTxtFileBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnTxtFileBrowse.TabIndex = 14;
+            this.btnTxtFileBrowse.Text = "Find...";
+            this.btnTxtFileBrowse.UseVisualStyleBackColor = true;
+            this.btnTxtFileBrowse.Click += new System.EventHandler(this.btnTxtFileBrowse_Click);
+            // 
+            // btnFileCheck
+            // 
+            this.btnFileCheck.Location = new System.Drawing.Point(787, 61);
+            this.btnFileCheck.Name = "btnFileCheck";
+            this.btnFileCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnFileCheck.TabIndex = 15;
+            this.btnFileCheck.Text = "File Load";
+            this.btnFileCheck.UseVisualStyleBackColor = true;
+            this.btnFileCheck.Click += new System.EventHandler(this.btnFileCheck_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 15);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Import Table";
+            // 
+            // txtTableNameBase
+            // 
+            this.txtTableNameBase.Location = new System.Drawing.Point(105, 27);
+            this.txtTableNameBase.Name = "txtTableNameBase";
+            this.txtTableNameBase.Size = new System.Drawing.Size(190, 23);
+            this.txtTableNameBase.TabIndex = 17;
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(23, 253);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(1032, 23);
+            this.progress.TabIndex = 18;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtMessage);
+            this.groupBox1.Controls.Add(this.btnImport);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtTableNameBase);
+            this.groupBox1.Location = new System.Drawing.Point(23, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1032, 126);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Database Import";
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(319, 26);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 18;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(105, 67);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ReadOnly = true;
+            this.txtMessage.Size = new System.Drawing.Size(749, 23);
+            this.txtMessage.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 70);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 15);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Progress";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 316);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.btnFileCheck);
+            this.Controls.Add(this.btnTxtFileBrowse);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtDb);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPw);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPw;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDb;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnTxtFileBrowse;
+        private System.Windows.Forms.Button btnFileCheck;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTableNameBase;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnImport;
+    }
+}
+
